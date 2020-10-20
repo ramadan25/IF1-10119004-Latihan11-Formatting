e5cc19a6fdbf71028758470d8d680827d6c1873c
ramadan25 <72897383+ramadan25@users.noreply.github.com>
2020-10-20 11:29:51 +0700

    Add files via upload

---
+++ IF1-10119004-Latihan11-Formatting/src/if1/pkg10119004/latihan11/formatting/IF110119004Latihan11Formatting.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119004.latihan11.formatting;
+
+/**
+ *
+ * @author ENDOG
+ * NAMA     : Muhamad Ramadan
+ * KELAS    : IF 1
+ * NIM      : 10119004
+ * Deskripsi Program : Program ini berisi untuk menampilkan
+ * value ke layar
+ */
+public class IF110119004Latihan11Formatting {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int i = 3546764;
+        int iMinus = -i;
+        System.out.println("i : " + i);
+        System.out.printf("%%d : %d%n", i);
+        System.out.printf("%%10d : %10d%n", i);
+        System.out.printf("%%+10d : %+10d%n", i);
+        System.out.printf("%%+10d : %+10d%n", iMinus);
+        System.out.printf("%%,10d : %,10d%n", i);
+        System.out.printf("%%-10d : %-10d%n", i);
+        double f = 5675482.982;
+        System.out.println("f : " + f);
+        System.out.printf("%%f : %f%n", f);
+        System.out.printf("%%.2f : %.2f%n", f);
+        System.out.printf("%%12.2f : %12.2f%n", f);
+        System.out.printf("%%,12.2f : %,12.2f%n", i);
+    }
+    
+}
